4e33c4e73c79655ef4ab468404b932a84d2e344e
Anil Yadav <anilyadav8149838058@gmail.com>
2025-04-04 16:28:10 +0530

    Recommended section

--- src/pages/video/Video.jsx
+++ src/pages/video/Video.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../components/playVideo/PlayVideo'
+import Recommended from '../../components/recommended/Recommended'
 
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo />
+      <Recommended/>
     </div>
   )
 }
